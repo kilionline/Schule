023742aaa4a7a2856b16cd5309eeae78fc63c2a5
KilianSchulPC <kilian@matheis-design.de>
2018-11-09 12:17:46 +0100

    Created class Mensch

---
+++ SchuleInfoUnterricht/src/schuleinfounterricht/Mensch.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schuleinfounterricht;
+
+import javafx.scene.paint.Color;
+
+
+/**
+ *
+ * @author ki.matheis
+ */
+public class Mensch {
+    //Attribute
+    private int age;
+    private int heightinCM;
+    private String name;
+    private String herkunft;
+    private Color  color;
+    
+    //Konstruktor
+    void Mensch(int agePara, int heightinCMPara, String namePara, String herkunftPara, Color colorPara){
+        age = agePara;
+        heightinCM = heightinCMPara;
+        name = namePara;
+        herkunft = herkunftPara;
+        color = colorPara;
+    }
+    //Methoden
+    void Erzeugen(){
+        
+    }
+}
